97a94c4f3c24a6396d74912b7c80b08ef5104632
jwshuff <16825267+JWShuff@users.noreply.github.com>
2021-07-16 14:08:17 -0400

    Basic category list and link to cat detail working.

--- craiglist_junior_app/urls.py
+++ craiglist_junior_app/urls.py
@@ -1,16 +1,17 @@
+from craiglist_junior_app.models import Category
 from django.urls import path
 from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home')
+# - `/categories`: A page listing all the categories
+    path('', views.home, name='home'),
+# - `/categories/:category_id`: A page to view the detail of a specific category and a list of all of its associated posts
+    path('<int:category_id>/', views.category_detail, name='category_detail'),
 ]
 
 
-# Here are a list of the routes you will need to build:
-# - `/categories`: A page listing all the categories
 # - `/categories/new`: A page with a form to create a new category
-# - `/categories/:category_id`: A page to view the detail of a specific category and a list of all of its associated posts
 # - `/categories/:category_id/edit`: A page with a form to update a specific category, with current values filled in already. Also include the ability to delete the specific category here. 
 # - `/categories/:category_id/posts/new`: A page with a form to create a new post, under the current category by default.
 # - `/categories/:category_id/posts/:post_id`: A page to view the detail of a specific post. Also include the ability go back to the parent category detail page (`/categories/:category_id/`)
--- craiglist_junior_app/views.py
+++ craiglist_junior_app/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from .models import Category, Post
 
-# Create your views here.
+def get_category(category_id):
+    return Category.objects.get(id=category_id)
+
+def get_post(post_id):
+    return Post.objects.get(id=post_id)
+
 def home(request):
-    return render(request, 'home.html')
+    categories = Category.objects.all()
+    return render(request, 'home.html', {'categories':categories})
+
+def category_detail(request, category_id):
+    return HttpResponse(f"Yep, category id {category_id} is working.")
