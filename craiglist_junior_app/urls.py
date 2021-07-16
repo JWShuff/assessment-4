e60c95587e5b2b98d151829e4f376a19b3a5d691
jwshuff <16825267+JWShuff@users.noreply.github.com>
2021-07-16 15:00:54 -0400

    Beginning to come together, still need to make posts work.

--- craiglist_junior_app/urls.py
+++ craiglist_junior_app/urls.py
@@ -4,15 +4,20 @@ from . import views
 
 
 urlpatterns = [
-# - `/categories`: A page listing all the categories
-    path('', views.home, name='home'),
-# - `/categories/:category_id`: A page to view the detail of a specific category and a list of all of its associated posts
+    # - `/categories`: A page listing all the categories
+    path('', views.category_list, name='home'),
+    # - `/categories/:category_id`: A page to view the detail of a specific category and a list of all of its associated posts
     path('<int:category_id>/', views.category_detail, name='category_detail'),
+    # - `/categories/new`: A page with a form to create a new category
+    path('new/', views.new_category, name='new_category'),
+    # - `/categories/:category_id/edit`: A page with a form to update a specific category, with current values filled in already. Also include the ability to delete the specific category here. 
+    path('<int:category_id>/edit/', views.edit_category, name='edit_category'),
+
+    # - `/categories/:category_id/posts/new`: A page with a form to create a new post, under the current category by default.
+    path('<int:category_id>/posts/new', views.new_post, name='new_post'),
+    # - `/categories/:category_id/posts/:post_id`: A page to view the detail of a specific post. Also include the ability go back to the parent category detail page (`/categories/:category_id/`)
+    path('<int:category_id>/posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    # - `/categories/:category_id/posts/:post_id/edit`: A page with a form to update a specific post, with current values filled in already. Also include the ability to delete the specific post here.
+    path('<int:category_id>/posts/<int:post_id>/edit/', views.edit_post, name='edit_post'),
 ]
 
-
-# - `/categories/new`: A page with a form to create a new category
-# - `/categories/:category_id/edit`: A page with a form to update a specific category, with current values filled in already. Also include the ability to delete the specific category here. 
-# - `/categories/:category_id/posts/new`: A page with a form to create a new post, under the current category by default.
-# - `/categories/:category_id/posts/:post_id`: A page to view the detail of a specific post. Also include the ability go back to the parent category detail page (`/categories/:category_id/`)
-# - `/categories/:category_id/posts/:post_id/edit`: A page with a form to update a specific post, with current values filled in already. Also include the ability to delete the specific post here.
--- craiglist_junior_app/views.py
+++ craiglist_junior_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, HttpResponse
 from .models import Category, Post
+from .forms import CategoryForm, PostForm
 
 def get_category(category_id):
     return Category.objects.get(id=category_id)
@@ -7,9 +8,48 @@ def get_category(category_id):
 def get_post(post_id):
     return Post.objects.get(id=post_id)
 
-def home(request):
+def category_list(request):
     categories = Category.objects.all()
     return render(request, 'home.html', {'categories':categories})
 
 def category_detail(request, category_id):
-    return HttpResponse(f"Yep, category id {category_id} is working.")
+    category= get_category(category_id)
+    return render(request, 'categories/category_detail.html', {'category':category})
+
+def new_category(request):
+    if request.method=="POST":
+        form=CategoryForm(request.POST)
+        if form.is_valid():
+            category=form.save(commit=False)
+            category.save()
+            return redirect('category_detail', category_id=category.id)
+    else:
+        form = CategoryForm
+    return render(request, 'categories/category_form.html', {'form':form, 'type_of_request':'New'})
+
+def edit_category(request, category_id):
+    category = get_category(category_id)
+    if request.method=="POST":
+        form=CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            category=form.save(commit=False)
+            category.save()
+            return redirect('category_detail', category_id=category.id)
+
+def delete_category(request, category_id):
+    pass
+
+# Post Methods
+
+def new_post(request):
+    pass
+
+def post_detail(request, category_id, post_id):
+    pass
+
+def edit_post(request, category_id, post_id):
+    pass
+
+def delete_post(request, category_id, post_id):
+    pass
+
